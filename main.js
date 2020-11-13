cde6274253c6a711e2759eb03c7e081584826fc0
emagdnimMQT <mark.w.novak@gmail.com>
2020-11-13 13:00:24 -0500

    validate cred function not working

--- main.js
+++ main.js
@@ -24,6 +24,15 @@ const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, inval
 
 
 // Add your functions below:
+const validateCred = (num) => {
+	let arr = (num + '').split('').reverse().map(x => parseInt(x));
+	let lastDigit = arr.splice(0, 1)[0];
+	let sum = arr.reduce((acc, val, i) => (i % 2 !== 0 ? acc + val : acc + ((val * 2) % 9) || 9), 0);
+	sum += lastDigit;
+	return sum % 10 === 0;
+};
+
+console.log(validateCred('4485275742308327'));
 console.log('hello world');
 
 
